9e5ad8634158683ce388359274ac4744bfd62b47
Alice <devalice117@gmail.com>
2026-01-29 15:51:27 -0300

    feat: create Partners section with colored SVGs and responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Contents } from './components/sections/Contents';
 import { Features } from './components/sections/Features';
 import { Gallery } from './components/sections/Galery';
 import { Hero } from './components/sections/Hero';
+import { Partners } from './components/sections/Partners';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Features/>
       <Contents/>
       <Gallery/>
+      <Partners/>
     </main>
   )
 }
